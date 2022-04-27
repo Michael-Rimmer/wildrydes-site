c9760ea8d5b5180924cf226409203ec1ac1101ac
Michael J. Rimmer <mjrimmer@qinetiq.com>
2022-04-27 17:22:51 +0100

    Update title

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-2_Ya8XWXmAW', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '235mijnvl6kkjp31te442n5b3c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'eu-west-2' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
